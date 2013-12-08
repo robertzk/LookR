09902fe3c053346dd7acf95747ac06fdb57d4821
Scott Hoover <Copied@Scotts-MacBook-Pro.local>
2013-12-08 00:03:00 -0800

    updated the handling of filters

--- sdk.R
+++ sdk.R
@@ -7,10 +7,6 @@
 # host = "demo.looker.com"
 # port = 443
 
-#####################
-# A ONE-OFF ATTEMPT #
-#####################
-
 # required packages
 require(RCurl)
 require(rjson)
@@ -29,13 +25,6 @@ LookerSetup = function(token, secret, host, port){
 	
 		Looker$port <- port
 	}
-
-LookerSetup(
-	token = "Mkz9GRYoIhyuJ898YG89Ig", 
-	secret = "v1+MNxMg1vdmljYbtBhEDFEQSlAUEZd4xWd", 
-	host = "demo.looker.com", 
-	port = 443
-)
 
 # LookerQuery constructs the API call
 LookerQuery = function(dictionary, query, fields, filters = NULL){
@@ -56,6 +45,7 @@ LookerQuery = function(dictionary, query, fields, filters = NULL){
 
 # allow for queries without filters #
 		if(is.null(filters)){
+
 			Looker$url <- paste(
 								"https://", 
 								Looker$host, 
@@ -64,7 +54,11 @@ LookerQuery = function(dictionary, query, fields, filters = NULL){
 								paste("fields=", Looker$field_list, sep=""), 
 								sep=""
 							)
-				} else {
+
+				} else {	
+
+			filter_list_clean <- filtersClean(filters)
+
 			Looker$url <- paste(
 								"https://", 
 								Looker$host, 
@@ -72,35 +66,28 @@ LookerQuery = function(dictionary, query, fields, filters = NULL){
 								"?", 
 								paste("fields=", Looker$field_list, sep=""), 
 								'&', 
-								paste(
-									"filters[", 
-									unlist(strsplit(filters, split=":"))[1], "]=", 
-									gsub(' ', '+', unlist(strsplit(filters, split=":"))[2]), 
-									sep=""), 
+								filter_list_clean, 
 								sep="")
 				}
-# TEST
 
-filter_list = strsplit(filters, split=":")
-if (length(filter_list)==1) {
-	# code for single filter
-} else {
-	filter_list_clean <- list()
-	for (i in 1:length(filter_list)) {
-			filter_list_clean[[i]] <- gsub(" ", "+", paste("filters[", filter_list[[i]][1], "]=", gsub("^[[:space:]]|[[:space:]]$", "", filter_list[[i]])[2], sep=""))
-	}
-filter_list_clean <- paste(unlist(filter_list_clean), collapse="&")
-}
-
-# END TEST
+		if(is.null(filters)){
 
 		Looker$StringToSign <- paste('GET', '\n',  
 										Looker$location, '\n', 
 										Looker$today, '\n',
 										Looker$nonce, '\n',
 										paste("fields=", Looker$field_list, sep=""), '\n',
-										paste("filters[", unlist(strsplit(filters, split=":"))[1], "]=", gsub(' ', '+', unlist(strsplit(filters, split=":"))[2]), sep=""), '\n',
-										sep = '')
+										sep = '')			
+		} else {
+
+		Looker$StringToSign <- paste('GET', '\n',  
+										Looker$location, '\n', 
+										Looker$today, '\n',
+										Looker$nonce, '\n',
+										paste("fields=", Looker$field_list, sep=""), '\n',
+										paste(gsub("&", "\n", filter_list_clean), "\n", sep=""),
+										sep = '')		
+		}
 
 		Looker$signature <- base64(hmac(Looker$secret, enc2utf8(Looker$StringToSign), algo="sha1", raw=TRUE), encode=TRUE)[1]
 		
@@ -117,6 +104,45 @@ filter_list_clean <- paste(unlist(filter_list_clean), collapse="&")
 return(Looker$results)
 }
 
+# in the case of one or more filters, filtersClean handles proper url formatting #
+filtersClean = function(filters){
+
+filter_list = strsplit(filters, split=":")
+
+		if(length(filter_list)==1){
+
+			filter_list_clean <- gsub(" ", "+", paste("filters[", filter_list[[1]][1], "]=", gsub("^[[:space:]]|[[:space:]]$", "", filter_list[[1]])[2], sep=""))
+
+		} else {
+
+			filter_list_clean <- list()
+
+			for (i in 1:length(filter_list)) {
+
+					filter_list_clean[[i]] <- gsub(" ", "+", paste("filters[", filter_list[[i]][1], "]=", gsub("^[[:space:]]|[[:space:]]$", "", filter_list[[i]])[2], sep=""))
+			}
+
+		filter_list_clean <- paste(unlist(filter_list_clean), collapse="&")
+	}
+return(filter_list_clean)
+}
+
+
+# TEST QUERIES #
+LookerSetup(
+	token = "Mkz9GRYoIhyuJ898YG89Ig", 
+	secret = "v1+MNxMg1vdmljYbtBhEDFEQSlAUEZd4xWd", 
+	host = "demo.looker.com", 
+	port = 443
+)
+
+LookerQuery(
+	dictionary="thelook", 
+	query="orders", 
+	fields=c("orders.count", "users.count", "users.created_month"), 
+	filters=c("orders.created_date:365 days", "users.created_date: 12 months ago for 10 months ")
+)
+
 LookerQuery(
 	dictionary="thelook", 
 	query="orders", 
@@ -128,17 +154,5 @@ LookerQuery(
 	dictionary="thelook", 
 	query="users", 
 	fields=c("users.age", "users.count"), 
-	filters=c("users.state:California, Nevada", "users.gender:m")
+	filters=c("orders.created_date:365 days")
 )
-
-setClass("Looker")
-useMethod("Setup", "Looker", Setup.Looker)
-Setup.Looker = function(token, secret, host, port){
-			Looker$token <- token
-	
-		Looker$secret <- secret
-	
-		Looker$host <- host
-	
-		Looker$port <- port
-}
