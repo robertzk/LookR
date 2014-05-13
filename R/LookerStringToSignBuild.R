505e345acc1fe74f2747a23002d281bd12b2e55b
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-05-12 19:21:54 -0500

    fix StringToSignBuild function

--- R/LookerStringToSignBuild.R
+++ R/LookerStringToSignBuild.R
@@ -1,76 +1,17 @@
 # LookerStringToSignBuild(...) constructs a url which is used as the main argument in the GET request 
-
 LookerStringToSignBuild = function(filters = Looker$filters, limit = Looker$limit){
-
-	location <- Looker$location
-	field_list <- Looker$field_list
-	today <- Looker$today
-	nonce <- Looker$nonce
-	filter_list_clean <- Looker$filter_list_clean
-
-	# case when filters IS NULL and limit IS NULL #
-		if(is.na(filters) && is.na(limit)){
-
-		StringToSign <- paste('GET', '\n',  
-										location, '\n', 
-										today, '\n',
-										nonce, '\n',
-										paste(
-											"fields=", 
-											field_list, 
-											sep=""), 
-											'\n',
-										sep="")	
-
-	# case when filters IS NULL and limit IS NOT NULL #
-				} else if(is.na(filters) && !is.na(limit)){
-
-		StringToSign <- paste('GET', 
-									'\n',  
-									location, '\n', 
-									today, '\n',
-									nonce, '\n',
-									paste("fields=", field_list, sep=""), 
-										'\n',
-									paste("&limit=", limit, sep=""), '\n',
-									sep = '')	
-
-	# case when filters IS NOT NULL and limit IS NULL #
-				} else if(!is.na(filters) && is.na(limit)){
-
-		StringToSign <- paste('GET', '\n',  
-										location, '\n', 
-										today, '\n',
-										nonce, '\n',
-										paste(
-											"fields=", 
-											field_list, 
-											sep=""), 
-											'\n',
-										paste(
-											gsub("&", "\n", filter_list_clean), 
-											"\n", 
-											sep=""),
-										sep = '')	
-
-	# case when filters IS NOT NULL and limit IS NOT NULL #		
-				} else {
-
-		StringToSign <- paste('GET', '\n',  
-										location, '\n', 
-										today, '\n',
-										nonce, '\n',
-										paste(
-											"fields=", 
-											field_list, 
-											sep=""), 
-											'\n',
-										paste(
-											gsub("&", "\n", filter_list_clean), 
-											"\n", 
-											sep=""),
-										paste("&limit=", limit, sep=""), '\n',
-										sep = '')	
-				}
-return(StringToSign)				
+  location <- Looker$location
+  field_list <- Looker$field_list
+  today <- Looker$today
+  nonce <- Looker$nonce
+  filter_list_clean <- Looker$filter_list_clean
+  request_lines <- c("GET", location, today, nonce)
+  request_lines <- c(request_lines, paste0("fields=", field_list))
+  if (!is.na(filters)[1])
+    request_lines <- c(request_lines, paste0(gsub("&", "\n", filter_list_clean)))
+  if (!is.na(limit))
+    request_lines <- c(request_lines, paste0("limit=", limit))
+  request_lines <- c(request_lines, c(""))
+  request_lines <- paste(request_lines, collapse = "\n")
+  request_lines
 }				
